ca7d318a5989b8d06b7630c8f04c39c5e2971073
vikalp <vikalpnimesh786@gmail.com>
2024-04-23 14:19:17 +0530

    why online therapy half done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,16 +6,18 @@ import OurExpertise from './Our Expertise/OurExpertise'
 import Quotes from './Quotes/Quotes'
 import Hero from './hero/Hero'
 import HowWorks from './how it works/HowWorks'
+import WhyTherapy from './why therapy/WhyTherapy'
 
 const Home = () => {
     return (
-        <div className="" >
-        <Header />
-        {/* <HowWorks/> */}
-        {/* <Hero/> */}
-        {/* <FAQ /> */}
-        {/* <OurExpertise /> */}
+      <div className="" >
+        <WhyTherapy/>
+        {/* <Header />
+        <Hero/>
+        <HowWorks/>
+        <OurExpertise />
         <Quotes/>
+        <FAQ /> */}
         {/* <Footer/> */}
     </div>
   )
